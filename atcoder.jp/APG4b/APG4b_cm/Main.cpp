13ee3120a2964c9baa4bfa82ee07ea81e8a98d01
shu8Cream <shu.vgage@gmail.com>
2020-09-08 13:27:30 +0900

    [AC] APG4b APG4b_cm

---
+++ atcoder.jp/APG4b/APG4b_cm/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  // ここにプログラムを追記
+  cout << "A:";
+  int i=0;
+  while(i<A){
+    cout << "]";
+    i++;
+  }
+  cout << endl << "B:";
+  i=0;
+  while(i<B){
+  	cout << "]";
+    i++;
+  }
+  cout << endl;
+}
