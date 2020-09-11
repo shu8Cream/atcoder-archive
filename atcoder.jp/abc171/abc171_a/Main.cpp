0ad86466a422a65b2e2cec5158d9db85551fe2df
shu8Cream <shu.vgage@gmail.com>
2020-09-11 18:26:18 +0900

    [AC] abc171 abc171_a

---
+++ atcoder.jp/abc171/abc171_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  char a;
+  cin >> a;
+  if(a<'a')
+  	cout << "A" << endl;
+  else
+    cout << "a" << endl;
+  
+}
