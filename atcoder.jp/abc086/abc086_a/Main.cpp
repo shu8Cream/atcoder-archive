91741b2b8111073f82a71a384017f628f8367bf5
shu8Cream <shu.vgage@gmail.com>
2020-09-14 11:23:29 +0900

    [AC] abc086 abc086_a

---
+++ atcoder.jp/abc086/abc086_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  int a,b;
+  cin >> a >> b;
+  int c=a*b;
+  if(c%2==0) cout << "Even" << endl;
+  else cout << "Odd" << endl;
+}
