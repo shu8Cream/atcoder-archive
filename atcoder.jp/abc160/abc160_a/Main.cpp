fb59f30b160f184c3947bf68ddf5b9ff5c8f69f7
shu8Cream <shu.vgage@gmail.com>
2020-09-15 21:50:55 +0900

    [AC] abc160 abc160_a

---
+++ atcoder.jp/abc160/abc160_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  string s;
+  cin>>s;
+  if(s[2]==s[3]&&s[4]==s[5]) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
