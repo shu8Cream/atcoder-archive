8ad9ddfa48cd0d80ccd6ead630e184bc7b0639a9
shu8Cream <shu.vgage@gmail.com>
2020-09-15 21:45:16 +0900

    [AC] abc162 abc162_a

---
+++ atcoder.jp/abc162/abc162_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  string n;
+  cin>>n;
+  if(n[0]=='7'||n[1]=='7'||n[2]=='7') cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
