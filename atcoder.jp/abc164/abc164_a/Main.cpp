0293526a628a0325e5d0bf4020b3c4543fdb7784
shu8Cream <shu.vgage@gmail.com>
2020-09-15 21:36:00 +0900

    [AC] abc164 abc164_a

---
+++ atcoder.jp/abc164/abc164_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const ll MX=1e18;
+
+int main(){
+  int s,w;
+  cin>>s>>w;
+  if(s>w) cout << "safe" << endl;
+  else cout << "unsafe" << endl;
+}
