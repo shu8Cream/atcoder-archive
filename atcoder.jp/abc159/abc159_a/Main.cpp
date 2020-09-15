31af848618559fc7386e1d266532f09c5b748854
shu8Cream <shu.vgage@gmail.com>
2020-09-15 21:58:54 +0900

    [AC] abc159 abc159_a

---
+++ atcoder.jp/abc159/abc159_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  int n,m;
+  cin>>n>>m;
+  cout << n*(n-1)/2+m*(m-1)/2 << endl;
+}
