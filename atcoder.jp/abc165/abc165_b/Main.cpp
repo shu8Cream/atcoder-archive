0e685014431074b3142835bc20005ab64caec6a3
shu8Cream <shu.vgage@gmail.com>
2020-09-16 14:44:07 +0900

    [AC] abc165 abc165_b

---
+++ atcoder.jp/abc165/abc165_b/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  ll x;
+  cin>>x;
+  ll a=100,ans=0;
+  while(x>a){
+    a=a+a/100;
+    ans++;
+  }
+  cout << ans << endl;
+}
