9bb72e16c765a7d989b4e3612ae799c106c41164
shu8Cream <shu.vgage@gmail.com>
2020-09-23 20:54:52 +0900

    [AC] abc152 abc152_a

---
+++ atcoder.jp/abc152/abc152_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  int n,m;
+  cin >> n >> m;
+  if(n-m>0) cout << "No" << endl;
+  else cout << "Yes" <<endl;
+}
