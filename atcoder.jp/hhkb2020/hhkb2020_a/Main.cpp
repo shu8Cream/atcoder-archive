1da275c065887cc176a1417254de7f9c1c1de57b
shu8Cream <shu.vgage@gmail.com>
2020-10-10 21:04:11 +0900

    [AC] hhkb2020 hhkb2020_a

---
+++ atcoder.jp/hhkb2020/hhkb2020_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  char s,t;
+  cin >> s >> t;
+  if(s=='Y') cout << (char)toupper(t) << endl;
+  else cout << t << endl;
+}
