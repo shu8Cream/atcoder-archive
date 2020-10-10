71b3853a3ef14adf315d73e402757cc3e16b5d27
shu8Cream <shu.vgage@gmail.com>
2020-10-10 20:15:29 +0900

    [AC] abc143 abc143_a

---
+++ atcoder.jp/abc143/abc143_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  int a,b;
+  cin >> a >> b;
+  
+  if(a-b*2>0) cout << a-b*2 << endl;
+  else cout << 0 << endl;
+}
