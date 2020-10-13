eaa1ccba0f79cea7df53bf464b869e39bdef1bf2
shu8Cream <shu.vgage@gmail.com>
2020-10-13 13:25:02 +0900

    [AC] abc178 abc178_d

---
+++ atcoder.jp/abc178/abc178_d/Main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+
+const int mod = 1000000007;
+//const int mod = 998244353;
+struct mint{
+  ll x;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main(){
+  int s;
+  cin >> s;
+  vector<mint> dp(s+1);
+  dp[0]=1;
+  for(int v=1; v<=s; v++){
+    rep(i,v-2) dp[v]+=dp[i];
+  }
+  cout << dp[s] << endl;
+}
