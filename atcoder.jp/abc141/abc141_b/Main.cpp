f28a7d7f0a8d0fe84128de895e822d2a2f846f5b
shu8Cream <shu.vgage@gmail.com>
2020-10-15 23:46:37 +0900

    [AC] abc141 abc141_b

---
+++ atcoder.jp/abc141/abc141_b/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+  string s;
+  cin >> s;
+  rep(i,s.size()){
+    if(i%2==0 && s[i]=='L'){
+      cout << "No" << endl;
+      return 0;
+    }
+    if(i%2==1 && s[i]=='R'){
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
+}
