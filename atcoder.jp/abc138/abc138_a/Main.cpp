d77a81eaea19d6240d31b2ed8c182a6d87f1e7e4
shu8Cream <shu.vgage@gmail.com>
2020-10-22 13:27:33 +0900

    [AC] abc138 abc138_a

---
+++ atcoder.jp/abc138/abc138_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+//using mint = modint998244353;
+
+int main(){
+  int a; string s;
+  cin >> a >> s;
+  if(a>=3200) cout << s << endl;
+  else cout << "red" << endl;
+}
