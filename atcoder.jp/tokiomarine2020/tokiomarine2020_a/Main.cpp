ee714198de326248ba146f4863bc2decf46d9b8b
shu8Cream <shu.vgage@gmail.com>
2020-10-23 15:32:51 +0900

    [AC] tokiomarine2020 tokiomarine2020_a

---
+++ atcoder.jp/tokiomarine2020/tokiomarine2020_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+//using mint = modint998244353;
+//using mint = modint1000000007;
+
+int main(){
+  string s;
+  cin >> s;
+  cout << s.substr(0,3) << endl;
+}
