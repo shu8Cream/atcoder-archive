eb0e5aa424e82b5c15c8403f80e01ffe98cafbe6
shu8Cream <shu.vgage@gmail.com>
2020-10-23 23:57:03 +0900

    [AC] abc130 abc130_a

---
+++ atcoder.jp/abc130/abc130_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int x,a;
+  cin >> x >> a;
+  if(x<a) cout << 0 << endl;
+  else cout << 10 << endl;
+}
