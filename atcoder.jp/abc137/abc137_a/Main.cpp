ec37cc8c66759f1e9c9f64825742d512be0b73a4
shu8Cream <shu.vgage@gmail.com>
2020-10-25 21:44:31 +0900

    [AC] abc137 abc137_a

---
+++ atcoder.jp/abc137/abc137_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int a,b;
+  cin >> a >> b;
+  cout << max(max(a+b, a-b), a*b) << endl;
+  
+}
