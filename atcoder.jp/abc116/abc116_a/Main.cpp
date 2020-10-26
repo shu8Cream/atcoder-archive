8f7dae41fccdf9c9a4a37590380766f0facad038
shu8Cream <shu.vgage@gmail.com>
2020-10-27 00:11:25 +0900

    [AC] abc116 abc116_a

---
+++ atcoder.jp/abc116/abc116_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int a,b,c;
+  cin >> a >> b >> c;
+  cout << a*b/2 << endl;
+}
