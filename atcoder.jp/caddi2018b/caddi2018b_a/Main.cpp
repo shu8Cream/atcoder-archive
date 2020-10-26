9b39f7de19accae7e94e2f7f599cd9f781cbb1f7
shu8Cream <shu.vgage@gmail.com>
2020-10-27 00:02:59 +0900

    [AC] caddi2018b caddi2018b_a

---
+++ atcoder.jp/caddi2018b/caddi2018b_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int n;
+  cin >> n;
+  int cnt=0;
+  rep(i,4){
+    if(n%10==2) cnt++;
+    n/=10;
+  }
+  cout << cnt << endl;
+}
