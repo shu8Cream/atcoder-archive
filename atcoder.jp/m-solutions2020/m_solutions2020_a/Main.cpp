0ecfce8745a9c4478a3435ddcbfb01a0013ceecd
shu8Cream <shu.vgage@gmail.com>
2020-10-26 14:33:45 +0900

    [AC] m-solutions2020 m_solutions2020_a

---
+++ atcoder.jp/m-solutions2020/m_solutions2020_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int x;
+  cin >> x;
+  cout << 10-x/200 << endl; 
+}
