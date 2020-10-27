eab0511a23e250a05386b7ea784502d05064c9b9
shu8Cream <shu.vgage@gmail.com>
2020-10-27 22:03:19 +0900

    [AC] abc061 abc061_a

---
+++ atcoder.jp/abc061/abc061_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int a,b,c;
+  cin >> a >> b >> c;
+  if(a<=c && b>=c) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
