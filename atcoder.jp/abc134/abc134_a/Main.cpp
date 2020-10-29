d2e3560968d3a4c6f063c344d2efcd0135450532
shu8Cream <shu.vgage@gmail.com>
2020-10-29 13:39:48 +0900

    [AC] abc134 abc134_a

---
+++ atcoder.jp/abc134/abc134_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  int r;
+  cin >> r;
+  cout << 3*r*r << endl;
+}
