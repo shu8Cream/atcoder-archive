0ea58990ad212c8caa3b95ab994a31c63a7c0571
shu8Cream <shu.vgage@gmail.com>
2020-10-29 14:18:17 +0900

    [AC] panasonic2020 panasonic2020_a

---
+++ atcoder.jp/panasonic2020/panasonic2020_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+const vi a = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51}; 
+
+int main(){
+  int k;
+  cin >> k;
+  cout << a[k-1] << endl;
+}
