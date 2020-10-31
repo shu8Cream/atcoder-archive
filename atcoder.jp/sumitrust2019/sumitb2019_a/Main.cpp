de79deee9ecc850ce051d781eac53b5a1962b34d
shu8Cream <shu.vgage@gmail.com>
2020-10-31 12:25:08 +0900

    [AC] sumitrust2019 sumitb2019_a

---
+++ atcoder.jp/sumitrust2019/sumitb2019_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+//using mint = modint1000000007;
+
+int main(){
+  int m1,d1,m2,d2;
+  cin >> m1 >> d1 >> m2 >> d2;
+  if(m1!=m2) cout << 1 << endl;
+  else cout << 0 << endl;
+}
