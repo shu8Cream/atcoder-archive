4a14995565066bd3c24e134e00b7db20dab16009
shu8Cream <shu.vgage@gmail.com>
2020-11-01 21:01:25 +0900

    [AC] abc181 abc181_a

---
+++ atcoder.jp/abc181/abc181_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i,n) for (int i=0; i<(n); i++)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using mint = modint998244353;
+//using mint = modint1000000007;
+
+int main(){
+  int n;
+  cin >> n;
+  if(n%2==0) cout << "White" << endl;
+  else cout << "Black" << endl;
+}
