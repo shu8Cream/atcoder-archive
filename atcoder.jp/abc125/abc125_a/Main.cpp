c3b702a4b3055a824c852b40bea3c0da646cb3f3
shu8Cream <shu.vgage@gmail.com>
2020-11-06 18:46:51 +0900

    [AC] abc125 abc125_a

---
+++ atcoder.jp/abc125/abc125_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+//using mint = modint998244353;
+using mint = modint1000000007;
+
+int main(){
+  int a,b,t;
+  cin >> a >> b >> t;
+  cout << t/a*b << endl;
+}
