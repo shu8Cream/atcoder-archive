b07667d1a37370078f9f9e980ece1509e4304a5e
shu8Cream <shu.vgage@gmail.com>
2020-11-08 21:01:58 +0900

    [AC] abc182 abc182_a

---
+++ atcoder.jp/abc182/abc182_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+//using mint = modint998244353;
+//using mint = modint1000000007;
+
+int main(){
+  int a,b;
+  cin >> a >> b;
+  cout << 2*a+100-b << endl;
+}
