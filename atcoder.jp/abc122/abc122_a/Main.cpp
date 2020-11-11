ec9d5480e3b06fe2f6422a2065054e1e3f21a883
shu8Cream <shu.vgage@gmail.com>
2020-11-11 15:14:01 +0900

    [AC] abc122 abc122_a

---
+++ atcoder.jp/abc122/abc122_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main(){
+  char b;
+  cin >> b;
+  if(b=='A') cout << "T" << endl;
+  if(b=='T') cout << "A" << endl;
+  if(b=='C') cout << "G" << endl;
+  if(b=='G') cout << "C" << endl;
+}
