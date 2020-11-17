34304e49bbe09a0d7c6d58009ec5b14f025eeebf
shu8Cream <shu.vgage@gmail.com>
2020-11-17 12:26:27 +0900

    [AC] abc117 abc117_a

---
+++ atcoder.jp/abc117/abc117_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main(){
+  double t,x;
+  cin >> t >> x;
+  printf("%.08lf", t/x);
+}
