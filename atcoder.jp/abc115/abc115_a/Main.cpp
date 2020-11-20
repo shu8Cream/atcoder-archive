390e76141af99db151acb1ef65ae5d25ca0d7ed8
shu8Cream <shu.vgage@gmail.com>
2020-11-20 17:19:48 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main(){
+  int d;
+  cin >> d;
+  cout << "Christmas";
+  rep(i,25-d) cout << " Eve";
+}
