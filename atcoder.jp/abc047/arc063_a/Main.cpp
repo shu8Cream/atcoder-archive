3e0e055474a022191695cdaf66d56e479079013d
shu8Cream <shu.vgage@gmail.com>
2020-11-26 21:16:17 +0900

    [AC] abc047 arc063_a

---
+++ atcoder.jp/abc047/arc063_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main(){
+  string s;
+  cin >> s;
+  int cnt=0;
+  rep(i,s.size()-1){
+    if(s[i]!=s[i+1]) cnt++;
+  }
+  cout << cnt << endl;
+}
