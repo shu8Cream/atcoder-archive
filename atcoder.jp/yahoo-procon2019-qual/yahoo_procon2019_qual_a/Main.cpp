a51709e77ca2882a615f57b0dfcc401362c3c366
shu8Cream <shu.vgage@gmail.com>
2020-11-27 22:53:17 +0900

    [AC] yahoo-procon2019-qual yahoo_procon2019_qual_a

---
+++ atcoder.jp/yahoo-procon2019-qual/yahoo_procon2019_qual_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main(){
+  int n,k;
+  cin >> n >> k;
+  if((n+1)/2>=k) cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
