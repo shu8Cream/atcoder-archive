93dc94eb19bea3f91e5b95fa9d14e0030c97cf86
shu8Cream <shu.vgage@gmail.com>
2020-12-01 19:19:01 +0900

    [AC] tenka1-2012-qualC tenka1_2012_9

---
+++ atcoder.jp/tenka1-2012-qualC/tenka1_2012_9/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main(){
+  int n;
+  cin >> n;
+  vi is_prime(n,1);
+  is_prime[0]=0,is_prime[1]=0;
+  for(int i=2; i<n; i++){
+    if(!is_prime[i]) continue;
+    for(int j=i*2; j<n; j+=i) is_prime[j]=0;
+  }
+  cout << accumulate(is_prime.begin(), is_prime.end(), 0) << endl;
+}
