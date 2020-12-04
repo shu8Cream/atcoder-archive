a5c16711e1a0362f2690a0bd990fd812a1b43f0f
shu8Cream <shu.vgage@gmail.com>
2020-12-04 21:04:25 +0900

    [AC] abc096 abc096_a

---
+++ atcoder.jp/abc096/abc096_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main(){
+  int n,m;
+  cin >> n >> m;
+  
+  if(n<=m) cout << n << endl;
+  else cout << n-1 << endl;
+}
