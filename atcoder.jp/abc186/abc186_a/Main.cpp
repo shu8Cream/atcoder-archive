a367cd3d0dc30de22f6dd9e3c125d0e255c30f9c
shu8Cream <shu.vgage@gmail.com>
2020-12-19 21:01:13 +0900

    [AC] abc186 abc186_a

---
+++ atcoder.jp/abc186/abc186_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 19.12.2020 20:53:43
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main() {
+    int n,w;
+    cin >> n >> w;
+    
+    int ans = n/w;
+    cout << ans << endl;
+}
