6d15a5675500428553508afeb7bc34257fb6afe6
shu8Cream <shu.vgage@gmail.com>
2020-12-21 18:33:04 +0900

    [AC] arc030 arc030_1

---
+++ atcoder.jp/arc030/arc030_1/Main.cpp
@@ -0,0 +1,14 @@
+/**
+*    author:  shu8Cream
+*    created: 21.12.2020 18:30:33
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin >> n >> k;
+    if(k*2<=n) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
