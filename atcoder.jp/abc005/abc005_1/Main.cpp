c530bb0a3fa8966e49483beab18920cc841d0366
shu8Cream <shu.vgage@gmail.com>
2020-12-22 11:50:25 +0900

    [AC] abc005 abc005_1

---
+++ atcoder.jp/abc005/abc005_1/Main.cpp
@@ -0,0 +1,19 @@
+/**
+*    author:  shu8Cream
+*    created: 22.12.2020 11:49:17
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    cout << y/x << endl;
+}
