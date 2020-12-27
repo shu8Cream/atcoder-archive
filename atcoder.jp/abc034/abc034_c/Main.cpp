06290d711e228c3f0cfcd651f165e42a4ebe5e7c
shu8Cream <shu.vgage@gmail.com>
2020-12-27 17:36:14 +0900

    [AC] abc034 abc034_c

---
+++ atcoder.jp/abc034/abc034_c/Main.cpp
@@ -0,0 +1,64 @@
+/**
+*    author:  shu8Cream
+*    created: 27.12.2020 17:24:26
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+const int mod = 1e9+7;
+
+struct mint{
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint f(ll n){
+    mint res = 1;
+    while(1){
+        if(n==0) break;
+        res*=n;
+        n-=1;
+    }
+    return res;
+}
+
+int main() {
+    int w,h;
+    cin >> w >> h;
+    w--; h--;
+    mint ans = f(w+h)/f(w)/f(h);
+    cout << ans << endl;
+}
