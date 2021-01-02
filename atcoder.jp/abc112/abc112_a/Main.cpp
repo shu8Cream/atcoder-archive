aa228ae05f3ae35b87ec751271517571396e9615
shu8Cream <shu.vgage@gmail.com>
2021-01-02 19:10:52 +0900

    [AC] abc112 abc112_a

---
+++ atcoder.jp/abc112/abc112_a/Main.cpp
@@ -0,0 +1,24 @@
+/**
+*    author:  shu8Cream
+*    created: 02.01.2021 19:09:20
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main() {
+    int n;
+    cin >> n;
+    if(n==1) cout << "Hello World" << endl;
+    else{
+        int a,b;
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+}
