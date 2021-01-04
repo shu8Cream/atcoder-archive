f71f8fda7f1e52fd8431ccd6bbb86d6d1b46d16d
shu8Cream <shu.vgage@gmail.com>
2021-01-04 20:15:04 +0900

    [AC] abc109 abc109_a

---
+++ atcoder.jp/abc109/abc109_a/Main.cpp
@@ -0,0 +1,15 @@
+/**
+*    author:  shu8Cream
+*    created: 04.01.2021 20:12:20
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b;
+    cin >> a >> b;
+    cout << (a*b%2!=0 ? "Yes" : "No") << endl;
+}
