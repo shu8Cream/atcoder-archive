cb24ea4ff9cc80b00fafc2524085aaee89ec885f
shu8Cream <shu.vgage@gmail.com>
2021-01-10 09:56:31 +0900

    [AC] arc111 arc111_a

---
+++ atcoder.jp/arc111/arc111_a/Main.cpp
@@ -0,0 +1,27 @@
+/**
+*    author:  shu8Cream
+*    created: 09.01.2021 20:57:19
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+ll mod_pow(ll a, ll b, ll n){
+    if(b==0) return 1;
+    if(b%2!=0) return a * mod_pow(a, b-1, n) % n;
+    else return mod_pow(a * a % n, b / 2, n) % n;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n,m;
+    cin >> n >> m;
+    cout << mod_pow(10,n,m*m) / m % m << endl;
+}
