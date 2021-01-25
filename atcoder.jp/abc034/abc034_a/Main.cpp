708d976695fcddb7f3c9f5ec993a33487195b42c
shu8Cream <shu.vgage@gmail.com>
2021-01-25 11:18:53 +0900

    [AC] abc034 abc034_a

---
+++ atcoder.jp/abc034/abc034_a/Main.cpp
@@ -0,0 +1,15 @@
+/**
+*    author:  shu8Cream
+*    created: 25.01.2021 11:17:09
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int x,y;
+    cin >> x >> y;
+    cout << ((x<y) ? "Better" : "Worse") << endl;
+}
