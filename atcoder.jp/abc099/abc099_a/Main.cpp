6e5f255eb1d629e1900e71534db03f8b78f47b44
shu8Cream <shu.vgage@gmail.com>
2021-02-09 15:37:02 +0900

    [AC] abc099 abc099_a

---
+++ atcoder.jp/abc099/abc099_a/Main.cpp
@@ -0,0 +1,15 @@
+/**
+*    author:  shu8Cream
+*    created: 09.02.2021 15:33:35
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    cout << (n>999 ? "ABD" : "ABC") << endl;
+}
