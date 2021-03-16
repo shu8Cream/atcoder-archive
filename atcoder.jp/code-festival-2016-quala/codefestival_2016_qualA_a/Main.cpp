30dc7898f33cfa5ac866f1feba0b41dabbb6d792
shu8Cream <shu.vgage@gmail.com>
2021-03-16 21:03:27 +0900

    [AC] code-festival-2016-quala codefestival_2016_qualA_a

---
+++ atcoder.jp/code-festival-2016-quala/codefestival_2016_qualA_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 16.03.2021 21:02:04
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string s;
+    cin >> s;
+    cout << s.substr(0,4) << " " << s.substr(4,8) << endl;
+}
