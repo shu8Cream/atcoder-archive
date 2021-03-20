9b31b12e4a3420fb74ba50bb164d27900018d389
shu8Cream <shu.vgage@gmail.com>
2021-03-20 21:01:23 +0900

    [AC] abc196 abc196_a

---
+++ atcoder.jp/abc196/abc196_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 20.03.2021 20:48:33
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << b-c << endl;
+}
