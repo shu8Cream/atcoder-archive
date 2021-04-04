51c4335b0dabbde901afe85e013c79521fc4db21
shu8Cream <shu.vgage@gmail.com>
2021-04-04 18:59:44 +0900

    [AC] arc039 arc039_b

---
+++ atcoder.jp/arc039/arc039_b/Main.cpp
@@ -0,0 +1,52 @@
+/**
+*    author:  shu8Cream
+*    created: 04.04.2021 17:43:35
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,k;
+    cin >> n >> k;
+    COMinit();
+    ll ans = 0;
+    if(n>k) ans=COM(n+k-1,k);
+    else{
+        k%=n;
+        ans=COM(n,k);
+    }
+    cout << ans << endl;
+}
