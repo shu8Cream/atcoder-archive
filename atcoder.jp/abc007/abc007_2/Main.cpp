52fdfdb5992a2540f593e2766160a6337d8ef765
shu8Cream <shu.vgage@gmail.com>
2021-04-10 21:16:00 +0900

    [AC] abc007 abc007_2

---
+++ atcoder.jp/abc007/abc007_2/Main.cpp
@@ -0,0 +1,24 @@
+/**
+*    author:  shu8Cream
+*    created: 10.04.2021 21:09:08
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string a;
+    cin >> a;
+    string ans;
+    if(a=="a") ans = "-1";
+    else ans = "a";
+    cout << ans << endl;
+}
