eb2155fe384bc465e78e3d2360748e456206bdf5
shu8Cream <shu.vgage@gmail.com>
2021-04-11 21:01:00 +0900

    [AC] abc198 abc198_a

---
+++ atcoder.jp/abc198/abc198_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 11.04.2021 20:46:00
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    cout << n-1 << endl;
+}
