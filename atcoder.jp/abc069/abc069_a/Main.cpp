e22a8a5594b9c29519b6532b9392f4544541a010
shu8Cream <shu.vgage@gmail.com>
2021-04-21 09:00:13 +0900

    [AC] abc069 abc069_a

---
+++ atcoder.jp/abc069/abc069_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 21.04.2021 08:59:04
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m;
+    cin >> n >> m;
+    cout << (n-1)*(m-1) << endl;
+}
