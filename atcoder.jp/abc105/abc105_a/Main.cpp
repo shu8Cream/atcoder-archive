4a827291f11d76fe813a451f901533276276da2f
shu8Cream <shu.vgage@gmail.com>
2021-04-24 20:26:32 +0900

    [AC] abc105 abc105_a

---
+++ atcoder.jp/abc105/abc105_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 24.04.2021 20:24:08
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,k;
+    cin >> n >> k;
+    cout << (n%k==0 ? 0 : 1) << endl;
+}
