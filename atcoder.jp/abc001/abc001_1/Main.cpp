334dcee86f03c1cbf8e0dc77813497edd0a92165
shu8Cream <shu.vgage@gmail.com>
2021-05-01 23:10:42 +0900

    [AC] abc001 abc001_1

---
+++ atcoder.jp/abc001/abc001_1/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 01.05.2021 23:10:07
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m;
+    cin >> n >> m;
+    cout << n-m << endl;
+}
