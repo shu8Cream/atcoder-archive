3500a0a8d4721e5ed9b49666e42e7ce3c88da6c1
shu8Cream <shu.vgage@gmail.com>
2021-05-06 21:03:48 +0900

    [AC] abc043 abc043_a

---
+++ atcoder.jp/abc043/abc043_a/Main.cpp
@@ -0,0 +1,22 @@
+/**
+*    author:  shu8Cream
+*    created: 06.05.2021 21:00:00
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    int ans = (n+1)*n/2;
+    cout << ans << endl;
+}
