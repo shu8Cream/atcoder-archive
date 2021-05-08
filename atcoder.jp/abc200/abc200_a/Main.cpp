54051497eecd5c8cbca1fe53a92c495b9005e202
shu8Cream <shu.vgage@gmail.com>
2021-05-08 21:00:56 +0900

    [AC] abc200 abc200_a

---
+++ atcoder.jp/abc200/abc200_a/Main.cpp
@@ -0,0 +1,21 @@
+/**
+*    author:  shu8Cream
+*    created: 08.05.2021 20:43:24
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    cout << (n+99)/100 << endl;
+}
