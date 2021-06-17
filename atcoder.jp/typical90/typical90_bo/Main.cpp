29ffe88a77ac63250183ee0c06bd21e15f6b2147
shu8Cream <shu.vgage@gmail.com>
2021-06-17 23:01:05 +0900

    [AC] typical90 typical90_bo

---
+++ atcoder.jp/typical90/typical90_bo/Main.cpp
@@ -0,0 +1,72 @@
+/**
+*    author:  shu8Cream
+*    created: 17.06.2021 22:49:25
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+class Radix {
+private:
+  const char* s;
+  int a[128];
+public:
+  Radix(const char* s = "0123456789ABCDEF") : s(s) {
+    int i;
+    for(i = 0; s[i]; ++i)
+      a[(int)s[i]] = i;
+  }
+  std::string to(long long p, int q) {
+    int i;
+    if(!p)
+      return "0";
+    char t[64] = { };
+    for(i = 62; p; --i) {
+      t[i] = s[p % q];
+      p /= q;
+    }
+    return std::string(t + i + 1);
+  }
+  std::string to(const std::string& t, int p, int q) {
+    return to(to(t, p), q);
+  }
+  long long to(const std::string& t, int p) {
+    int i;
+    long long sm = a[(int)t[0]];
+    for(i = 1; i < (int)t.length(); ++i)
+      sm = sm * p + a[(int)t[i]];
+    return sm;
+  }
+};
+
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string n; ll k; 
+    cin >> n >> k;
+    Radix r;
+    rep(i,k){
+        string a = r.to(n,8,9);
+        rep(j,a.size()) if(a[j]=='8') a[j]='5';
+        n = a; 
+    }
+    cout << n << endl;
+}
