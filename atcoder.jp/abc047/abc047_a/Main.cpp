b350e024a88f6ef1134d137ce2e26ba40084796a
shu8Cream <shu.vgage@gmail.com>
2021-06-24 21:39:32 +0900

    [AC] abc047 abc047_a

---
+++ atcoder.jp/abc047/abc047_a/Main.cpp
@@ -0,0 +1,34 @@
+/**
+*    author:  shu8Cream
+*    created: 24.06.2021 21:29:10
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b,c;
+    cin >> a >> b >> c;
+    bool f = false;
+    if(a+b==c || a+c==b || b+c==a) f = true;
+    cout << (f ? "Yes" : "No") << endl;
+}
