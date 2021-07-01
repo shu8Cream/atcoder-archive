88b8b838c729f14c6304abd3a25a00df862d7384
shu8Cream <shu.vgage@gmail.com>
2021-07-01 23:06:52 +0900

    [AC] typical90 typical90_bq

---
+++ atcoder.jp/typical90/typical90_bq/Main.cpp
@@ -0,0 +1,42 @@
+/**
+*    author:  shu8Cream
+*    created: 01.07.2021 22:42:06
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+const int mod = 1e9+7;
+
+ll mod_pow(ll a, ll b, ll n){
+    if(b==0) return 1;
+    if(b%2!=0) return a * mod_pow(a, b-1, n) % n;
+    else return mod_pow(a * a % n, b / 2, n) % n;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n,k; cin >> n >> k;
+    if(k==1) cout << (n==1 ? 1 : 0) << endl;
+    else if(n==1) cout << k%mod << endl;
+    else if(n==2) cout << k*(k-1)%mod << endl;
+    else cout << k*(k-1)%mod *mod_pow(k-2,n-2,mod)%mod << endl;
+}
