3682199272168ca777ba10aac36ce119c60c123d
shu8Cream <shu.vgage@gmail.com>
2021-07-10 21:01:19 +0900

    [AC] abc209 abc209_a

---
+++ atcoder.jp/abc209/abc209_a/Main.cpp
@@ -0,0 +1,34 @@
+/**
+*    author:  shu8Cream
+*    created: 10.07.2021 20:40:50
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b;
+    cin >> a >> b;
+    int ans = b-a+1;
+    if(ans<0) ans = 0;
+    cout << ans << endl;
+}
