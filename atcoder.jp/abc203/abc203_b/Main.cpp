02d604fc206c3bcc07b5848a1faef4b66bd7dedb
shu8Cream <shu.vgage@gmail.com>
2021-07-10 13:21:03 +0900

    [AC] abc203 abc203_b

---
+++ atcoder.jp/abc203/abc203_b/Main.cpp
@@ -0,0 +1,32 @@
+/**
+*    author:  shu8Cream
+*    created: 10.07.2021 10:40:42
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,k; cin >> n >> k;
+    int ans = (1+n)*n/2*k*100 + (1+k)*k/2*n;
+    cout << ans << endl;
+}
