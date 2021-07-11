47836e256a64fdb10fb081462de4fb3d9993c94e
shu8Cream <shu.vgage@gmail.com>
2021-07-11 22:58:38 +0900

    [AC] abc096 abc096_b

---
+++ atcoder.jp/abc096/abc096_b/Main.cpp
@@ -0,0 +1,36 @@
+/**
+*    author:  shu8Cream
+*    created: 11.07.2021 22:53:17
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll a,b,c,k;
+    cin >> a >> b >> c >> k;
+    ll ans = 0;
+    if(a>=b && a>=c) ans = b+c+a*pow(2,k);
+    if(b>=a && b>=c) ans = a+c+b*pow(2,k);
+    if(c>=b && c>=a) ans = b+a+c*pow(2,k);
+    cout << ans << endl;
+}
