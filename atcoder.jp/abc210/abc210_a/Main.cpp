1bc8451535537af419ad1aabe2cd5d8f5eeb4911
shu8Cream <shu.vgage@gmail.com>
2021-07-17 21:02:40 +0900

    [AC] abc210 abc210_a

---
+++ atcoder.jp/abc210/abc210_a/Main.cpp
@@ -0,0 +1,34 @@
+/**
+*    author:  shu8Cream
+*    created: 17.07.2021 20:50:07
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n,a,x,y;
+    cin >> n >> a >> x >> y;
+    int ans = min(a,n)*x;
+    if(n>a) ans += y*(n-a);
+    cout << ans << endl;
+}
