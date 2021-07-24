71c4aade3a71fb89d6d391c1f916c08df348c8f2
shu8Cream <shu.vgage@gmail.com>
2021-07-24 21:02:13 +0900

    [AC] abc211 abc211_a

---
+++ atcoder.jp/abc211/abc211_a/Main.cpp
@@ -0,0 +1,34 @@
+/**
+*    author:  shu8Cream
+*    created: 24.07.2021 20:10:43
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    double a,b;
+    cin >> a >> b;
+    double ans = a+2.0*b;
+    ans/=3.0;
+    printf("%.15lf\n", ans);
+}
