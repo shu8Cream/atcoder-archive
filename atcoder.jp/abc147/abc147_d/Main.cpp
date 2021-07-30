9b8c8044503a19c4be42a129d6c19670caa8c043
shu8Cream <shu.vgage@gmail.com>
2021-07-30 22:07:05 +0900

    [AC] abc147 abc147_d

---
+++ atcoder.jp/abc147/abc147_d/Main.cpp
@@ -0,0 +1,80 @@
+/**
+*    author:  shu8Cream
+*    created: 29.07.2021 23:30:34
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+const int mod = 1000000007;
+struct mint{
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vi a(n); rep(i,n) cin >> a[i];
+    mint ans = 0;
+    vi p(62),q(62);
+    ll pow2 = 1;
+    rep(d,62){
+        rep(i,n){
+            if((pow2&a[i])==pow2) p[d]++;
+            else q[d]++;
+        }
+        pow2*=2;
+    }
+    pow2 = 1;
+    rep(i,62){
+        ans += (mint)pow2*p[i]*q[i];
+        pow2 *= 2;
+    }
+    cout << ans << endl;
+}
