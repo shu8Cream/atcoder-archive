5a4a1c9cef5da7a36547ef430b170dd1d842831b
shu8Cream <shu.vgage@gmail.com>
2021-08-08 21:03:46 +0900

    [AC] abc213 abc213_a

---
+++ atcoder.jp/abc213/abc213_a/Main.cpp
@@ -0,0 +1,36 @@
+/**
+*    author:  shu8Cream
+*    created: 08.08.2021 20:45:52
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b; cin >> a >> b;
+    rep(i,256){
+        if((a^i)==b){
+            cout << i << endl;
+            return 0;
+        }
+    }
+}
