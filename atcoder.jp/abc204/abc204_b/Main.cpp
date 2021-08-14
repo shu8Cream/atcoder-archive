25400fada86622d3d9ba0fc474f774f8f7077de1
shu8Cream <shu.vgage@gmail.com>
2021-08-14 13:58:04 +0900

    [AC] abc204 abc204_b

---
+++ atcoder.jp/abc204/abc204_b/Main.cpp
@@ -0,0 +1,34 @@
+/**
+*    author:  shu8Cream
+*    created: 14.08.2021 13:54:51
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vi a(n); rep(i,n) cin >> a[i];
+    int ans = 0;
+    rep(i,n) if(a[i]>10) ans+=a[i]-10;
+    cout << ans << endl;
+}
