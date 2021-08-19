a2ad1939c73eb5e2d02f64f78dd89368244da62c
shu8Cream <shu.vgage@gmail.com>
2021-08-19 22:15:01 +0900

    [AC] abc072 abc072_a

---
+++ atcoder.jp/abc072/abc072_a/Main.cpp
@@ -0,0 +1,33 @@
+/**
+*    author:  shu8Cream
+*    created: 19.08.2021 22:13:19
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll x,t; cin >> x >> t;
+    
+    ll ans = max(x-t,0LL);
+    cout << ans << endl;
+}
