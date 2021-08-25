076098de47881e3ba77d3a8714e94fbf117b4ef4
shu8Cream <shu.vgage@gmail.com>
2021-08-25 21:01:14 +0900

    [AC] abc214 abc214_a

---
+++ atcoder.jp/abc214/abc214_a/Main.cpp
@@ -0,0 +1,33 @@
+/**
+*    author:  shu8Cream
+*    created: 25.08.2021 20:44:39
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    if(n<=125) cout << 4 << endl;
+    else if(n<=211) cout << 6 << endl;
+    else cout << 8 << endl;
+}
