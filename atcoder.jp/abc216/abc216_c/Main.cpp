ab7833fd592a291a99f59c20ef60b0671a99d9b4
shu8Cream <shu.vgage@gmail.com>
2021-08-29 21:16:57 +0900

    [AC] abc216 abc216_c

---
+++ atcoder.jp/abc216/abc216_c/Main.cpp
@@ -0,0 +1,44 @@
+/**
+*    author:  shu8Cream
+*    created: 29.08.2021 20:56:17
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n; cin >> n;
+    string ans = "";
+    while(1){
+        if(n==0) break;
+        if(n%2==1){
+            ans += "A";
+            n--;
+        }else{
+            ans += "B";
+            n/=2;
+        }
+    }
+    reverse(all(ans));
+    cout << ans << endl;
+}
