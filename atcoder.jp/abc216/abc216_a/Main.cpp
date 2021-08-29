2089c67818e85f6b2357f39de5d1cecb49786ed6
shu8Cream <shu.vgage@gmail.com>
2021-08-29 21:03:24 +0900

    [AC] abc216 abc216_a

---
+++ atcoder.jp/abc216/abc216_a/Main.cpp
@@ -0,0 +1,36 @@
+/**
+*    author:  shu8Cream
+*    created: 29.08.2021 20:56:02
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    double a; cin >> a;
+    int y = int(a*10)%10;
+    int x = int(a*10)/10;
+    if(y<=2) cout << x << "-" << endl;
+    else if(y<=6) cout << x << endl;
+    else cout << x << "+" << endl;
+}
