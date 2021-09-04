8af80516f5d6c1e428fbf3b637549bc7937e1ffe
shu8Cream <shu.vgage@gmail.com>
2021-09-04 21:06:49 +0900

    [AC] abc217 abc217_c

---
+++ atcoder.jp/abc217/abc217_c/Main.cpp
@@ -0,0 +1,36 @@
+/**
+*    author:  shu8Cream
+*    created: 04.09.2021 20:45:53
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vi p(n); rep(i,n) cin >> p[i];
+    vi q(n);
+    rep(i,n) q[p[i]-1] = i+1;
+    rep(i,n) cout << q[i] << " ";
+    cout << endl;
+}
