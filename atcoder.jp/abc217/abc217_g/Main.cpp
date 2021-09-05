7151f62cd2b0c4bd259785aff773f6ee7f7d14e9
shu8Cream <shu.vgage@gmail.com>
2021-09-05 23:40:47 +0900

    [AC] abc217 abc217_g

---
+++ atcoder.jp/abc217/abc217_g/Main.cpp
@@ -0,0 +1,75 @@
+/**
+*    author:  shu8Cream
+*    created: 04.09.2021 21:53:07
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+const int mod = 998244353;
+struct mint{
+    ll x;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint dp[5050][5050];
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m; cin >> n >> m;
+    dp[0][0]=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j]*(mint)(j-(i-1)/m);
+        }
+    }
+
+    for(int i=1; i<=n; i++){
+        cout << dp[n][i] << endl;
+    }
+}
