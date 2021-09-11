1f9b05e850e2e988158e5f61b2e3b7fa0cec867f
shu8Cream <shu.vgage@gmail.com>
2021-09-11 21:04:45 +0900

    [AC] abc218 abc218_b

---
+++ atcoder.jp/abc218/abc218_b/Main.cpp
@@ -0,0 +1,34 @@
+/**
+*    author:  shu8Cream
+*    created: 11.09.2021 20:40:21
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    vi p(26);
+    rep(i,26) cin >> p[i];
+    rep(i,26) cout << char('a' + p[i]-1);
+    cout << endl;
+}
