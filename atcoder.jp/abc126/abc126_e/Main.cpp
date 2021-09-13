4b8b32310716788f5d64e3f05fcb809639747db0
shu8Cream <shu.vgage@gmail.com>
2021-09-13 21:28:27 +0900

    [AC] abc126 abc126_e

---
+++ atcoder.jp/abc126/abc126_e/Main.cpp
@@ -0,0 +1,64 @@
+/**
+*    author:  shu8Cream
+*    created: 13.09.2021 21:10:00
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+struct UnionFind{
+    vector<int> par;
+    vector<map<int, int>> mp;
+    UnionFind(int n=0): par(n, -1), mp(n) {}
+    int find(int x){
+        if(par[x] < 0) return x;
+        return par[x] = find(par[x]);
+    }
+    bool merge(int x, int y){
+        x = find(x); y = find(y);
+        if(x == y) return false;
+        if(par[x] > par[y]) swap(x,y); //マージテク
+        for(auto p : mp[y]){
+            mp[x][p.first] += p.second;
+        }
+        mp[y] = map<int,int>(); //メモリの解放
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    bool same(int x, int y) {return find(x) == find(y);}
+    int size(int x) {return -par[find(x)];}
+};
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m; cin >> n >> m;
+    UnionFind uf(n);
+    rep(i,m){
+        int x,y,z; cin >> x >> y >> z;
+        x--; y--;
+        uf.merge(x,y);
+    }
+    set<int> s;
+    rep(i,n) s.insert(uf.find(i));
+    cout << sz(s) << endl;
+}
