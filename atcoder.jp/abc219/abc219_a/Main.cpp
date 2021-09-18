b04e27d44b38fd90625be9a296e6e6423b147698
shu8Cream <shu.vgage@gmail.com>
2021-09-18 21:02:16 +0900

    [AC] abc219 abc219_a

---
+++ atcoder.jp/abc219/abc219_a/Main.cpp
@@ -0,0 +1,35 @@
+/**
+*    author:  shu8Cream
+*    created: 18.09.2021 20:47:55
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int x; cin >> x;
+    if(x<40) cout << 40-x << endl;
+    else if(x<70) cout << 70-x << endl;
+    else if(x<90) cout << 90-x << endl;
+    else cout << "expert" << endl;
+}
