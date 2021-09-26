f6424f7ff8645c9ba54a19fd9342929cd3877fc3
shu8Cream <shu.vgage@gmail.com>
2021-09-26 21:01:23 +0900

    [AC] abc220 abc220_a

---
+++ atcoder.jp/abc220/abc220_a/Main.cpp
@@ -0,0 +1,54 @@
+/**
+*    author:  shu8Cream
+*    created: 26.09.2021 20:03:18
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b,c;
+    cin >> a >> b >> c;
+    for(int i=a; i<=b; i++){
+        if(i%c==0){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+}
