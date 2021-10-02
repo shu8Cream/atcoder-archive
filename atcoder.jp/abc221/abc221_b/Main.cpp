ecd1fd806d255ad453fee67047287f0b797938fe
shu8Cream <shu.vgage@gmail.com>
2021-10-02 21:04:29 +0900

    [AC] abc221 abc221_b

---
+++ atcoder.jp/abc221/abc221_b/Main.cpp
@@ -0,0 +1,61 @@
+/**
+*    author:  shu8Cream
+*    created: 02.10.2021 20:31:11
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string s,t;
+    cin >> s >> t;
+    int n = sz(s);
+    if(s==t){
+        cout << "Yes" << endl;
+        return 0;
+    }
+    rep(i,n-1){
+        string ss = s;
+        swap(ss[i],ss[i+1]);
+        if(ss==t){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
