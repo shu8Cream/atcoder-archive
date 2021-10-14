9627548bf2ba766407003ebe97d7e043317114fe
shu8Cream <shu.vgage@gmail.com>
2021-10-14 23:34:51 +0900

    [AC] soundhound2018-summer-qual soundhound2018_summer_qual_a

---
+++ atcoder.jp/soundhound2018-summer-qual/soundhound2018_summer_qual_a/Main.cpp
@@ -0,0 +1,49 @@
+/**
+*    author:  shu8Cream
+*    created: 14.10.2021 23:32:55
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b; cin >> a >> b;
+    if(a*b==15) cout << "*" << endl;
+    else if(a+b==15) cout << "+" << endl;
+    else cout << "x" << endl;
+}
