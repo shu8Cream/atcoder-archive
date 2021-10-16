76833b38b04f0b429ac159256a0767947190a86d
shu8Cream <shu.vgage@gmail.com>
2021-10-16 20:47:35 +0900

    [AC] chokudai_S002 chokudai_S002_f

---
+++ atcoder.jp/chokudai_S002/chokudai_S002_f/Main.cpp
@@ -0,0 +1,53 @@
+/**
+*    author:  shu8Cream
+*    created: 16.10.2021 20:44:31
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    set<P> s;
+    rep(i,n){
+        ll a,b; cin >> a >> b;
+        if(a>b) swap(a,b);
+        s.insert({a,b});
+    }
+    cout << sz(s) << endl;
+}
