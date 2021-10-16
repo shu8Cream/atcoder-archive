cd68be686a3602ec0fb2a90a626e040181059f07
shu8Cream <shu.vgage@gmail.com>
2021-10-16 10:37:24 +0900

    [AC] chokudai_S001 chokudai_S001_h

---
+++ atcoder.jp/chokudai_S001/chokudai_S001_h/Main.cpp
@@ -0,0 +1,58 @@
+/**
+*    author:  shu8Cream
+*    created: 16.10.2021 10:36:10
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int LIS(vi &a){
+    int n = sz(a);
+    vi dp(n,INF);
+    rep(i,n){
+        auto itr = lower_bound(all(dp), a[i]);
+        *itr = a[i];
+    }
+    return lower_bound(all(dp), INF) - dp.begin();
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vi a(n); rep(i,n) cin >> a[i];
+    cout << LIS(a) << endl;
+}
