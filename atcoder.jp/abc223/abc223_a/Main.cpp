31e3a77934dc5c950bb220bac8d2972f732f2104
shu8Cream <shu.vgage@gmail.com>
2021-10-17 21:01:51 +0900

    [AC] abc223 abc223_a

---
+++ atcoder.jp/abc223/abc223_a/Main.cpp
@@ -0,0 +1,48 @@
+/**
+*    author:  shu8Cream
+*    created: 17.10.2021 20:46:17
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll x; cin >> x;
+    if(x!=0 && x%100==0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
