d2fcefedc375b0b9076a04b47eb445dbdf233606
shu8Cream <shu.vgage@gmail.com>
2021-10-23 20:47:36 +0900

    [AC] aising2019 aising2019_b

---
+++ atcoder.jp/aising2019/aising2019_b/Main.cpp
@@ -0,0 +1,55 @@
+/**
+*    author:  shu8Cream
+*    created: 23.10.2021 20:42:25
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,a,b;
+    cin >> n >> a >> b;
+    vi p(n); rep(i,n) cin >> p[i];
+    ll cnt1 = 0, cnt2 = 0, cnt3 = 0;
+    rep(i,n){
+        if(p[i]<=a) cnt1++;
+        else if(p[i]<=b) cnt2++;
+        else cnt3++;
+    }
+    cout << min({cnt1,cnt2,cnt3}) << endl;
+}
