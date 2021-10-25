0ef74d2b062a80e535494d9d46c1f7a5e17fbd83
shu8Cream <shu.vgage@gmail.com>
2021-10-25 23:55:20 +0900

    [AC] abc224 abc224_f

---
+++ atcoder.jp/abc224/abc224_f/Main.cpp
@@ -0,0 +1,107 @@
+/**
+*    author:  shu8Cream
+*    created: 25.10.2021 19:06:47
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+const int mod = 998244353;
+struct mint{
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+ll mod_pow(ll a, ll b, ll n){
+    if(b==0) return 1;
+    if(b%2!=0) return a * mod_pow(a, b-1, n) % n;
+    else return mod_pow(a * a % n, b / 2, n) % n;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string s; cin >> s;
+    int n = sz(s);
+    if(n==1){
+        cout << s << endl;
+        return 0;
+    }
+    vector<mint> k2(n),a(n);
+    k2[0] = mint(2).pow(n-1);
+    rep(i,n) if(i) k2[i] = k2[i-1]/2;
+    a[0] = mint(2).pow(n-1);
+    mint t = 1;
+    rep(i,n-1){
+        a[i+1] = a[i] - t*k2[i+1];
+        t*=10;
+        a[i+1] += t*k2[i+1];
+    }
+    mint ans = 0;
+    reverse(all(s));
+    rep(i,n){
+        mint c = s[i]-'0';
+        ans += c*a[i];
+    }
+    cout << ans << endl;
+}
