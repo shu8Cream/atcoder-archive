fe1dbc92b00c567bf16a31e086c29aeaef3b2dc4
shu8Cream <shu.vgage@gmail.com>
2021-11-07 21:06:00 +0900

    [AC] abc226 abc226_b

---
+++ atcoder.jp/abc226/abc226_b/Main.cpp
@@ -0,0 +1,57 @@
+/**
+*    author:  shu8Cream
+*    created: 07.11.2021 20:52:28
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vvi L(n);
+    rep(i,n){
+        int l; cin >> l;
+        rep(j,l){
+            int a; cin >> a;
+            L[i].push_back(a);
+        }
+    }
+    set<vi> se;
+    rep(i,n) se.insert(L[i]);
+    cout << sz(se) << endl;
+}
