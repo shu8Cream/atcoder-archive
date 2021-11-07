f4ff57b45e5c54f9a8fa695b7aaaa4bc58262a29
shu8Cream <shu.vgage@gmail.com>
2021-11-07 21:17:17 +0900

    [AC] abc226 abc226_c

---
+++ atcoder.jp/abc226/abc226_c/Main.cpp
@@ -0,0 +1,93 @@
+/**
+*    author:  shu8Cream
+*    created: 07.11.2021 20:52:35
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+vi bfs(int s, vvi &to){
+    int n = sz(to);
+    vi dist(n, -1);
+    queue<int> q;
+    dist[s] = 0;
+    q.push(s);
+
+    while (!q.empty()) {
+        int v = q.front(); q.pop();
+        for (int nv : to[v]) {
+            if (dist[nv] != -1) continue;
+            dist[nv] = dist[v] + 1;
+            q.push(nv);
+        }
+    }
+    return dist;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vvi to(n);
+    vi time(n);
+    rep(i,n){
+        cin >> time[i];
+        int k; cin >> k;
+        rep(j,k){
+            int a; cin >> a;
+            if(a==0) continue;
+            a--;
+            to[i].push_back(a);
+        }
+    }
+    ll ans = 0;
+
+    vi dist(n, -1);
+    queue<int> q;
+    dist[n-1] = 0;
+    q.push(n-1);
+
+    while (!q.empty()) {
+        int v = q.front(); q.pop();
+        for (int nv : to[v]) {
+            if (dist[nv] != -1) continue;
+            dist[nv] = dist[v] + 1;
+            q.push(nv);
+        }
+    }
+    rep(i,n) if(dist[i]!=-1) ans += time[i];
+    cout << ans << endl;
+}
