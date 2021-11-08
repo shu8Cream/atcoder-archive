954b344b8fba58cf33dd639c855968125f29aa28
shu8Cream <shu.vgage@gmail.com>
2021-11-08 12:09:59 +0900

    [AC] abc226 abc226_e

---
+++ atcoder.jp/abc226/abc226_e/Main.cpp
@@ -0,0 +1,137 @@
+/**
+*    author:  shu8Cream
+*    created: 07.11.2021 21:34:44
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+const int mod = 998244353;
+struct mint{
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+struct UnionFind{
+    vector<int> par;
+    vector<map<int, int>> mp;
+    UnionFind(int n=0): par(n, -1), mp(n) {}
+    int find(int x){
+        if(par[x] < 0) return x;
+        return par[x] = find(par[x]);
+    }
+    bool merge(int x, int y){
+        x = find(x); y = find(y);
+        if(x == y) return false;
+        if(par[x] > par[y]) swap(x,y);
+        for(auto p : mp[y]){
+            mp[x][p.first] += p.second;
+        }
+        mp[y] = map<int,int>(); //メモリの解放
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    bool same(int x, int y) {return find(x) == find(y);}
+    int size(int x) {return -par[find(x)];}
+};
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m; cin >> n >> m;
+    UnionFind uf(n);
+    int cnt = 0;
+    set<int> se;
+    rep(i,m){
+        int a,b; cin >> a >> b;
+        a--; b--;
+        if(uf.same(a,b) && !se.count(uf.find(a))){
+            cnt++;
+            se.insert(uf.find(a));
+        }else if(uf.same(a,b) && se.count(uf.find(a))){
+            cout << 0 << endl;
+            return 0;
+        }
+        int tmp = uf.find(a);
+        int tmp2 = uf.find(b);
+        uf.merge(a,b);
+        if(se.count(tmp) && tmp!=uf.find(a)){
+            se.erase(tmp);
+            se.insert(uf.find(a));
+        }
+        if(se.count(tmp2) && tmp2!=uf.find(a)){
+            se.erase(tmp2);
+            se.insert(uf.find(a));
+        }
+    }
+    set<int> gr;
+    mint ans = mint(2).pow(cnt);
+    if(cnt==0) ans = 0;
+    rep(i,n) gr.insert(uf.find(i));
+    if(cnt!=gr.size()){
+        cout << 0 << endl;
+        return 0;
+    }
+    cout << ans << endl;
+}
