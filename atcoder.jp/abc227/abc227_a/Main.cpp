e886a0a5bf5ba65d828ae21d1954142534857e9d
shu8Cream <shu.vgage@gmail.com>
2021-11-13 21:05:58 +0900

    [AC] abc227 abc227_a

---
+++ atcoder.jp/abc227/abc227_a/Main.cpp
@@ -0,0 +1,52 @@
+/**
+*    author:  shu8Cream
+*    created: 13.11.2021 20:48:59
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,k,a;
+    cin >> n >> k >> a;
+    int ans = 0;
+    rep(i,k){
+        ans = (a-1+i)%n;
+    }
+    cout << ans+1 << endl;
+}
