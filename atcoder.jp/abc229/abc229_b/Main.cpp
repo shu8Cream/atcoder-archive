62732748d72ce2ab77c9c5b04ccbc23cababece8
shu8Cream <shu.vgage@gmail.com>
2021-11-27 21:04:49 +0900

    [AC] abc229 abc229_b

---
+++ atcoder.jp/abc229/abc229_b/Main.cpp
@@ -0,0 +1,56 @@
+/**
+*    author:  shu8Cream
+*    created: 27.11.2021 20:50:02
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+cout << H << " ";
+debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string a,b; cin >> a >> b;
+    reverse(all(a));
+    reverse(all(b));
+    int n = min(sz(a), sz(b));
+    bool f = false;
+    rep(i,n){
+        int A = a[i]-'0';
+        int B = b[i]-'0';
+        if(A+B>=10) f = true;
+    }
+    cout << (f ? "Hard" : "Easy") << endl;
+}
