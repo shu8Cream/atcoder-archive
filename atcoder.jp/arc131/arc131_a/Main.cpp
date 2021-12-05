ed00b5a1996bcf4a9118705f323f9a1ed046c711
shu8Cream <shu.vgage@gmail.com>
2021-12-05 21:49:07 +0900

    [AC] arc131 arc131_a

---
+++ atcoder.jp/arc131/arc131_a/Main.cpp
@@ -0,0 +1,66 @@
+/**
+*    author:  shu8Cream
+*    created: 05.12.2021 21:23:00",
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T>
+string to_string(T v) {
+  if (v.empty()) return "{}";
+  string ret = "{";
+  for (auto x : v) ret += to_string(x) + ",";
+  ret.back() = '}';
+  return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+  return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll a,b; cin >> a >> b;
+    string ans = "";
+    if(b%2!=0) b*=10;
+    b/=2; 
+    ans += to_string(a);
+    ans += "0";
+    ans += to_string(b);
+    cout << ans << endl;
+}
