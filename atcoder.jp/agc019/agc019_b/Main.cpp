fe73c03c870198f69898417e8c4a7f2a207e86c5
shu8Cream <shu.vgage@gmail.com>
2021-12-24 00:44:56 +0900

    [AC] agc019 agc019_b

---
+++ atcoder.jp/agc019/agc019_b/Main.cpp
@@ -0,0 +1,71 @@
+/**
+*    author:  shu8Cream
+*    created: 24.12.2021 00:04:19
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string a; cin >> a;
+    ll n = sz(a);
+    map<char,ll> mp;
+    for(char c:a) mp[c]++;
+    ll ans = n*(n-1)/2+1;
+    for(auto[k,v]:mp) ans-=v*(v-1)/2;
+    cout << ans << endl;
+}
