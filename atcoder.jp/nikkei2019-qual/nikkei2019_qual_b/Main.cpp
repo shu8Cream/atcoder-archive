c916dd32d61c26055f8f3d27abbe2928fefc2f3d
shu8Cream <shu.vgage@gmail.com>
2022-01-05 22:55:12 +0900

    [AC] nikkei2019-qual nikkei2019_qual_b

---
+++ atcoder.jp/nikkei2019-qual/nikkei2019_qual_b/Main.cpp
@@ -0,0 +1,74 @@
+/**
+*    author:  shu8Cream
+*    created: 05.01.2022 22:51:31
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<string> a(3);
+    rep(i,3) cin >> a[i];
+    int ans = 0;
+    rep(i,n){
+        if(a[0][i]==a[1][i] && a[0][i]==a[2][i]) continue;
+        if(a[0][i]==a[1][i] || a[0][i]==a[2][i] || a[1][i]==a[2][i]) ans++;
+        else ans+=2;
+    }
+    cout << ans << endl;
+}
