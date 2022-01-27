6279889725722b0668d4134b57edf670fe44bd43
shu8Cream <shu.vgage@gmail.com>
2022-01-27 12:32:01 +0900

    [AC] abc133 abc133_e

---
+++ atcoder.jp/abc133/abc133_e/Main.cpp
@@ -0,0 +1,118 @@
+/**
+*    author:  shu8Cream
+*    created: 25.01.2022 21:24:46
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+const int mod = 1000000007;
+struct mint{
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n,k; cin >> n >> k;
+    vvi to(n);
+    rep(i,n-1){
+        int a,b; cin >> a >> b;
+        a--; b--;
+        to[a].push_back(b);
+        to[b].push_back(a);
+    }
+    mint ans = k;
+    vector<mint> cnt(100100);
+    auto dfs = [&](auto self, int s, int d, int p)->void{
+        for(auto v:to[s]){
+            if(v==p) continue;
+            cnt[s]+=1;
+            if(d==0) ans*=mint(k)-cnt[s];
+            else ans*=mint(k)-1-cnt[s];
+            self(self,v,d+1,s);
+        }
+    };
+    dfs(dfs,0,0,-1);
+    cout << ans << endl;
+}
