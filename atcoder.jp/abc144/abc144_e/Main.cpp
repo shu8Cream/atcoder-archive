d3c8522a8de355772bda8e4920c2c34ce39b3483
shu8Cream <shu.vgage@gmail.com>
2022-01-29 11:05:31 +0900

    [AC] abc144 abc144_e

---
+++ atcoder.jp/abc144/abc144_e/Main.cpp
@@ -0,0 +1,88 @@
+/**
+*    author:  shu8Cream
+*    created: 28.01.2022 12:40:07
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n,k; cin >> n >> k;
+    vi a(n),f(n);
+    rep(i,n) cin >> a[i];
+    rep(i,n) cin >> f[i];
+    sort(rall(a));
+    sort(all(f));
+
+    debug(a);
+    debug(f);
+
+    auto check = [&](ll x)->bool{
+        ll cnt = 0;
+        rep(i,n) cnt+=max(a[i]-x/f[i],0LL);
+        if(cnt<=k) return true;
+        else return false;
+    };
+
+    ll ng = -1, ok = 1e18;
+    while(ok-ng>1){
+        ll md = (ng+ok)/2;
+        if(check(md)) ok = md;
+        else ng = md;
+    }
+    cout << ok << endl;
+}
