a5c1ec6c77dbc7c9c2aed3e6c3e01f0853d7f5fa
shu8Cream <shu.vgage@gmail.com>
2022-02-05 21:09:15 +0900

    [AC] abc238 abc238_b

---
+++ atcoder.jp/abc238/abc238_b/Main.cpp
@@ -0,0 +1,80 @@
+/**
+*    author:  shu8Cream
+*    created: 05.02.2022 20:49:30
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n; cin >> n;
+    vi a; a.push_back(0);
+    int now = 0;
+    rep(i,n){
+        int x; cin >> x;
+        now+=x;
+        now%=360;
+        a.push_back(now);
+    }
+    sort(all(a));
+    ll ans = 0;
+    rep(i,sz(a)-1){
+        chmax(ans,a[i+1]-a[i]);
+    }
+    chmax(ans,360-a[sz(a)-1]);
+    cout << ans << endl;
+}
