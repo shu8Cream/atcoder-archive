15b795e605523487a17ee3a239e10dc1f2fe28f0
shu8Cream <shu.vgage@gmail.com>
2022-02-15 10:49:12 +0900

    [AC] arc135 arc135_b

---
+++ atcoder.jp/arc135/arc135_b/Main.cpp
@@ -0,0 +1,120 @@
+/**
+*    author:  shu8Cream
+*    created: 13.02.2022 20:26:56
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+vi rndvi(int l, int r, int len){
+    std::random_device rnd;
+    std::mt19937 mt(rnd());
+    std::uniform_int_distribution<> rand(l, r);
+    vi res(len);
+    rep(i,len) res[i] = rand(mt);
+    return res;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vi s(n); 
+    // 自前
+    // vi A = rndvi(0,20,12);
+    // rep(i,n) s[i] = A[i]+A[i+1]+A[i+2];
+    // debug(A);
+    // debug(s);
+
+    // 入力
+    rep(i,n) cin >> s[i];
+    // 32 37 31 31 41 44 29 10 19 20
+
+    ////////////////////////
+
+    vi d(n-1); rep(i,n-1) d[i] = s[i+1]-s[i];
+    vvi r(3);
+    rep(i,n-1){
+        ll t = 0;
+        if(!r[i%3].empty()) t = r[i%3].back();
+        r[i%3].push_back(t+d[i]);
+    }
+    vi a(n+2);
+    rep(i,3){
+        ll mn = 0;
+        rep(j,sz(r[i])) chmin(mn,r[i][j]);
+        a[i] = abs(mn);
+    }
+    bool f = true;
+    rep(i,3){
+        a[i] = s[0]-a[(i+1)%3]-a[(i+2)%3];
+        if(!(a[0]<0 || a[1]<0 || a[2]<0)) break;
+    }
+    debug(a[0],a[1],a[2]);
+    // a[2] = -a[0]-a[1];
+    if(a[0]<0 || a[1]<0 || a[2]<0) f = false;
+    rep(i,n-1) a[i+3] = a[i%3]+r[i%3][i/3];
+    rep(i,n+2)if(a[i]<0) f = false;
+
+    rep(i,n)if(s[i]!=a[i]+a[i+1]+a[i+2]) f = false;
+
+    cout << (f ? "Yes":"No") << endl;
+
+    debug(a);
+    if(f){
+        rep(i,n+2) cout << a[i] << " ";
+        cout << endl;
+    }
+}
