b3d2e95f0f0490c6b923c9da6a7a148751ca819e
shu8Cream <shu.vgage@gmail.com>
2022-03-17 12:40:41 +0900

    [AC] agc005 agc005_b

---
+++ atcoder.jp/agc005/agc005_b/Main.cpp
@@ -0,0 +1,81 @@
+/**
+*    author:  shu8Cream
+*    created: 08.03.2022 22:56:28
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n; cin >> n;
+    vi pos(n+1),a(n);
+    rep(i,n){
+        cin >> a[i];
+        pos[a[i]] = i+1;
+    }
+    ll ans = 0;
+    set<ll> used = {0,n+1};
+    for(int i=1; i<=n; i++){
+        ll r,l;
+        auto it = used.lower_bound(pos[i]);
+        r = *it;
+        l = *(--it);
+        ans+=i*(pos[i]-l) * (r-pos[i]);
+        used.insert(pos[i]);
+    }
+    cout << ans << endl;
+}
