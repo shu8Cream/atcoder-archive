262a82e215cd5e1797dd564a6289e9137a4934e8
shu8Cream <shu.vgage@gmail.com>
2022-03-20 17:17:12 +0900

    [AC] abc241 abc241_b

---
+++ atcoder.jp/abc241/abc241_b/Main.cpp
@@ -0,0 +1,75 @@
+/**
+*    author:  shu8Cream
+*    created: 20.03.2022 17:11:42
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n,m; cin >> n >> m;
+    vi a(n); rep(i,n) cin >> a[i];
+    vi b(m); rep(i,m) cin >> b[i];
+    multiset<ll> ms;
+    rep(i,n) ms.insert(a[i]);
+    bool f = true;
+    rep(i,m){
+        if(!ms.count(b[i])) f = false;
+        else ms.erase(ms.find(b[i]));
+    }
+    cout << (f ? "Yes" : "No") << endl;
+}
