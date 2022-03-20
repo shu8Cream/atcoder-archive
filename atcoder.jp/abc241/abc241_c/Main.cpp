dd47300828ed744a04a687dd3ca7ea2166646e2a
shu8Cream <shu.vgage@gmail.com>
2022-03-20 17:29:33 +0900

    [AC] abc241 abc241_c

---
+++ atcoder.jp/abc241/abc241_c/Main.cpp
@@ -0,0 +1,94 @@
+/**
+*    author:  shu8Cream
+*    created: 20.03.2022 17:11:51
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define overload3(a,b,c,d,...) d
+#define rep1(i,n) for (int i=0; i<(n); i++)
+#define rep2(i,a,n) for (int i=(a); i<(n); i++)
+#define rep(...) overload3(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rrep1(i,n) for (int i=(n-1); i>=0; i--)
+#define rrep2(i,a,n) for (int i=(n-1); i>=(a); i--)
+#define rrep(...) overload3(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+using ll = long long;
+using P = pair<ll,ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll INF = 8e18;
+
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll n; cin >> n;
+    vector<string> s(n); rep(i,n) cin >> s[i];
+    bool f = false;
+    rep(i,n)rep(j,max(n-5,0LL)){
+        int cnt = 0;
+        rep(k,6)if(s[i][j+k]=='#') cnt++;
+        if(cnt>3) f = true;
+    }
+    rep(i,n)rep(j,max(n-5,0LL)){
+        int cnt = 0;
+        rep(k,6)if(s[j+k][i]=='#') cnt++;
+        if(cnt>3) f = true;
+    }
+    rep(i,max(n-5,0LL))rep(j,max(n-5,0LL)){
+        int cnt = 0;
+        rep(k,6)if(s[i+k][j+k]=='#') cnt++;
+        if(cnt>3) f = true;
+    }
+    rep(i,n) reverse(all(s[i]));
+    rep(i,max(n-5,0LL))rep(j,max(n-5,0LL)){
+        int cnt = 0;
+        rep(k,6)if(s[i+k][j+k]=='#') cnt++;
+        if(cnt>3) f = true;
+    }
+    cout << (f ? "Yes" : "No") << endl;
+}
