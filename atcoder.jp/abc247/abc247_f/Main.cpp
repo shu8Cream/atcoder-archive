9667f4fc3a53b651441a93337c3216b7f3f3002a
shu8Cream <shu.vgage@gmail.com>
2022-04-29 01:21:34 +0900

    [AC] abc247 abc247_f

---
+++ atcoder.jp/abc247/abc247_f/Main.cpp
@@ -0,0 +1,164 @@
+/**
+*    author:  shu8Cream
+*    created: 27.04.2022 22:39:04
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define overload3(a,b,c,d,...) d
+#define rep1(i,n) for (int i=0; i<(n); i++)
+#define rep2(i,a,n) for (int i=(a); i<(n); i++)
+#define rep(...) overload3(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rrep1(i,n) for (int i=(n-1); i>=0; i--)
+#define rrep2(i,a,n) for (int i=(n-1); i>=(a); i--)
+#define rrep(...) overload3(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define pcnt __builtin_popcountll
+using ll = long long;
+using P = pair<ll,ll>;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vi = vc<ll>;
+using vvi = vv<ll>;
+const ll INF = 8e18;
+template<typename T>istream& operator>>(istream&i,vc<T>&v){rep(j,sz(v))i>>v[j];return i;}
+template<typename T>string join(const T&v,const string& d=""){stringstream s;rep(i,sz(v))(i?s<<d:s)<<v[i];return s.str();}
+template<typename T>ostream& operator<<(ostream&o,const vc<T>&v){if(sz(v))o<<join(v," ");return o;}
+template<typename T1,typename T2>istream& operator>>(istream&i,pair<T1,T2>&v){return i>>v.first>>v.second;}
+template<typename T1,typename T2>ostream& operator<<(ostream&o,const pair<T1,T2>&v){return o<<v.first<<","<<v.second;}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+const int mod = 998244353;
+struct mint{
+    ll x;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+
+    bool operator==(const mint &other) const {
+        return x == other.x;
+    }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+struct UnionFind{
+    vector<int> par;
+    vector<map<int, int>> mp;
+    UnionFind(int n=0): par(n, -1), mp(n) {}
+    int find(int x){
+        if(par[x] < 0) return x;
+        return par[x] = find(par[x]);
+    }
+    bool merge(int x, int y){
+        x = find(x); y = find(y);
+        if(x == y) return false;
+        if(par[x] > par[y]) swap(x,y); //マージテク
+        for(auto p : mp[y]){
+            mp[x][p.first] += p.second;
+        }
+        mp[y] = map<int,int>(); //メモリの解放
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    bool same(int x, int y) {return find(x) == find(y);}
+    int size(int x) {return -par[find(x)];}
+};
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    int n; cin >> n;
+    UnionFind uf(n);
+    vi p(n); cin >> p;
+    vi q(n); cin >> q;
+    rep(i,n) p[i]--;
+    rep(i,n) q[i]--;
+    vvi to(n);
+    rep(i,n){
+        to[p[i]].push_back(q[i]);
+        to[q[i]].push_back(p[i]);
+        uf.merge(p[i],q[i]);
+    }
+    map<int,vi> grp;
+    rep(i,n) grp[uf.find(i)].push_back(i);
+    debug(grp);
+    mint ans = 1;
+    vc<mint> r(n+1);
+    r[1] = 1;
+    r[2] = 3;
+    rep(i,3,n+1) r[i] = r[i-1] + r[i-2];
+    for(auto[id,vc] : grp){
+        int eg = 0;
+        for(auto p:vc) eg += sz(to[p]);
+        eg/=2; debug(eg);
+        ans *= r[eg];
+    }
+    cout << ans << endl;
+}
